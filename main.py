3d21b1132692d8a86afd0b907171e8443e93501b
thisisanro <antonromanenko@tutanota.com>
2025-07-14 14:41:47 +0000

    Add --verbose flag to print prompt and token usage stats

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -9,19 +10,31 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    try:
-        prompt = sys.argv[1]
-    except IndexError:
-        print("Error: You must provide a prompt")
-        exit(1)
+    args = sys.argv[1:]
+    is_verbose = False
+    if "--verbose" in args:
+        is_verbose = True
+        args.remove("--verbose")
+
+    if not args:
+        print("Please enter your prompt.")
+        print("Example: python main.py 'your prompt here'")
+        sys.exit(1)
+    prompt = " ".join(args)
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
+
+    if is_verbose:
+        print(f"User prompt: {prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
     print(response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
