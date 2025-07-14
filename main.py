3ffc46cce2988dce37bffb93aaa60ddbd4b3e70d
thisisanro <antonromanenko@tutanota.com>
2025-07-14 13:13:23 +0000

    Add command line argument for prompt input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,9 +8,16 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    try:
+        prompt = sys.argv[1]
+    except IndexError:
+        print("Error: You must provide a prompt")
+        exit(1)
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=prompt,
     )
     print(response.text)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
