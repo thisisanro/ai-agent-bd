ab7e2a1ef5aae27341d1a6bf8bf0704847e0606e
thisisanro <antonromanenko@tutanota.com>
2025-07-22 14:40:49 +0000

    Add write_file method and tests for it

--- tests.py
+++ tests.py
@@ -1,13 +1,6 @@
-from functions.get_files_info import get_file_content
+from functions.write_file import write_file
 
-print('\nTest 1: Read main.py')
-print(get_file_content("calculator", "main.py"))
+print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
-print('\nTest 2: Read pkg/calculator.py')
-print(get_file_content("calculator", "pkg/calculator.py"))
-
-print('\nTest 3: Attempt to read /bin/cat')
-print(get_file_content("calculator", "/bin/cat"))
-
-print('\nTest 4: Attempt to read non-existent file')
-print(get_file_content("calculator", "pkg/does_not_exist.py"))
