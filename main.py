872cbcfe867c25f4e0faee5206a2a78706194407
thisisanro <antonromanenko@tutanota.com>
2025-07-22 15:19:56 +0000

    Add schema for get_files_info and integrate Gemini function calling

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import sys
+import json
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
+from functions.get_files_info import schema_get_files_info
 
 def main():
     load_dotenv()
@@ -24,21 +25,42 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
     )
 
     if is_verbose:
         print(f"User prompt: {prompt}")
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    
-    print(response.text)
 
+    if response.function_calls:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
+
